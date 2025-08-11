3546c6a6e759252a5e2e970ab31ef6b5b1feeb3a
Ritesh Gohil <riteshgohil.25@gmail.com>
2025-08-11 11:32:00 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))  # Adjust if you have code in src/
+sys.path.insert(0, os.path.abspath(os.path.join('..', 'src')))
 
 # -- Project information -----------------------------------------------------
 project = 'PWNCLOUDOS'
